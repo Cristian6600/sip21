22d2b669e468bea9317838d77c4e92bd28341d25
Cristian6600 <cristian6600@hotmail.es>
2022-10-23 19:36:30 -0500

    1

--- sip21/settings/local.py
+++ sip21/settings/local.py
@@ -2,25 +2,25 @@ from .base import *
 import os 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 import dj_database_url
 from decouple import config
 
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default=config('DATABASE_URL')
-#     )
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(os.path.join(BASE_DIR, "db.sqlite3"))     
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': str(os.path.join(BASE_DIR, "db.sqlite3"))     
+#     }
+# }
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
